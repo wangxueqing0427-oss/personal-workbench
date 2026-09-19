14404cc2d79d8f59c5abbdf0dc8e41d500b615c1
wangxueqing0427-oss <wangxueqing0427@gmail.com>
2026-09-19 22:20:00 +0800

    Create sw-v066s.js

---
+++ sw-v066s.js
@@ -0,0 +1,5 @@
+﻿const CACHE_NAME = "personal-workbench-v066s-followup-plan";
+const ASSETS = ["./v066s.html", "./app-v066s.js", "./style.css?v=066", "./manifest.json"];
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener("fetch", event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => { const copy = response.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)); return response; }))));
