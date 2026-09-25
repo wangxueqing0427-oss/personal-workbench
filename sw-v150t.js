be9526648310b437bd295794778d5cec263b3055
wangxueqing0427-oss <wangxueqing0427@gmail.com>
2026-09-25 22:11:46 +0800

    Add V1.5.0 streamlined workbench sw-v150t.js

---
+++ sw-v150t.js
@@ -0,0 +1,5 @@
+const CACHE='personal-workbench-v150t',FILES=['./v150t.html?v=1500925','./app-v150t.js?v=1500925','./style.css?v=1500925'];
+self.addEventListener('install',function(event){event.waitUntil(caches.open(CACHE).then(function(cache){return cache.addAll(FILES)}).then(function(){return self.skipWaiting()}))});
+self.addEventListener('activate',function(event){event.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(key){return key.indexOf('personal-workbench-')===0&&key!==CACHE}).map(function(key){return caches.delete(key)}))}).then(function(){return self.clients.claim()}))});
+self.addEventListener('fetch',function(event){if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(function(response){if(response.ok){var copy=response.clone();caches.open(CACHE).then(function(cache){cache.put(event.request,copy)})}return response}).catch(function(){return caches.match(event.request)}))});
+
